e98df6218d14437579111e8dd0efc565544dfd36
ssbaj <ssb9018@ajou.ac.kr>
2026-01-03 13:36:39 +0900

    first commit

---
+++ R/e_dfcalss.R
@@ -0,0 +1,11 @@
+e_dfclass<-function(explaining=0){
+
+  if(explaining==0) {
+    cat("  ", '\n')
+    cat("\033[1;32m## Statistics for PA and AI dataset ----------- \033[0m", '\n')
+    cat("  행정통계와 AI 데이터셋 ", '\n')
+    cat("  1) salary_data ", '\n')
+    cat("  2) therapie ", '\n')
+    cat("   ", '\n')
+  } }
+
